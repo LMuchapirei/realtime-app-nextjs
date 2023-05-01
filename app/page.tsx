029e0f1daf3fbee1dd06ec734c8fff645cded7f3
Linval Muchapirei <h170407h@hit.ac.zw>
2023-05-01 12:53:58 +0200

    feat: Database integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { db } from '@/lib/db'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default async function Home() {
+  await db.set("hello","hola")
   return (
     <div className='text-red-500'>Hello World</div>
   )
---
+++ lib/db.ts
@@ -0,0 +1,11 @@
+import { Redis } from '@upstash/redis'
+
+
+const url = process.env.UPSTASH_REDIS_REST_URL || ""
+const token = process.env.UPSTASH_REDIS_REST_TOKEN || ""
+
+
+export const db = new Redis({
+    token:token,
+    url:url
+})
